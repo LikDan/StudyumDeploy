664fdbf849d383742d5ef355ed8dafb03e1fe66d
LikDan <likdan.company@gmail.com>
2023-10-15 00:46:18 +0300

    Initial commit

--- sources/frontend/pull.sh
+++ sources/frontend/pull.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-git -C repository pull
+echo "Nothing to do"
 
-cd repository
-if [ -n "$1" ]; then
-    docker build -t frontend:latest -t backend:$1 -f deploy/Dockerfile .
-else
-    docker build -t frontend:latest -f deploy/Dockerfile .
-fi
+#git -C repository pull
+#
+#cd repository
+#if [ -n "$1" ]; then
+#    docker build -t frontend:latest -t backend:$1 -f deploy/Dockerfile .
+#else
+#    docker build -t frontend:latest -f deploy/Dockerfile .
+#fi
--- update.sh
+++ update.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 if [ -n "$1" ]; then
-    (cd ./sources/$1 && ./pull.sh $2)
+    (cd ./sources/"$1" && ./pull.sh "$2")
 else
     (cd ./sources/frontend && ./pull.sh)
     (cd ./sources/backend && ./pull.sh)
 fi
 
-cd app/deploy
+cd app/deploy || exit
 
 docker compose down
-docker compose build
 docker compose up -d
 docker compose logs -f
